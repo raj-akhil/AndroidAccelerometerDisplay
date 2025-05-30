393d7ce2218619c298e17fa00c4229fb0acd7185
raj-akhil <akhilraj.nos@gmail.com>
2025-05-30 02:36:38 +0100

    -change the layout little bit then added switch and new theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
